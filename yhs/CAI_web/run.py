ab4d9faf0a53dbc18d840061be904f8912e0ddb6
yhs <slmteruto@gmail.com>
2020-03-31 14:22:42 +0900

    web project update

--- yhs/CAI_web/run.py
+++ yhs/CAI_web/run.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template('main.html')
+    return render_template('CAI_main.html')
 
+############# main ######################
 @app.route("/main/")
 def main() :
-    return render_template('web_main_test.html')
+    return render_template('CAI_main.html')
+
+
+## upload
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f'static/uploads/{secure_filename(f.filename)}')
+        return render_template('CAI_main.html')
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
